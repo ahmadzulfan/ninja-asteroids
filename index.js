dd80df9dbdab9f7b8378ee9cbc0797158dff11de
ahmadzulfan <ahmadzulfanmoney@gmail.com>
2025-01-07 08:23:23 +0700

    move and rotate player

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-c.fillStyle = 'black';
-c.fillRect(0, 0, canvas.width, canvas.height);
-
 class Player {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
+        this.rotation = 0
     }
 
     draw() {
+        c.save();
+
+        c.translate(this.position.x, this.position.y);
+        c.rotate(this.rotation);
+        c.translate(-this.position.x, -this.position.y);
+
         c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
         c.fillStyle = 'red';
         c.fill();
         // c.fillStyle = 'red';
         // c.fillRect(this.position.x, this.position.y, 100, 100);
+        c.beginPath();
         c.moveTo(this.position.x + 30, this.position.y);
         c.lineTo(this.position.x - 10, this.position.y - 10);
         c.lineTo(this.position.x - 10, this.position.y + 10);
@@ -26,6 +31,13 @@ class Player {
 
         c.strokeStyle = 'white'
         c.stroke();
+        c.restore();
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
     }
 }
 
@@ -33,6 +45,72 @@ const player = new Player({
     position: {x: canvas.width/2, y: canvas.height/2},
     velocity: {x: 0, y: 0},
 });
-player.draw()
 
-console.log(player);
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+const SPEED = 3;
+const ROTATION_SPEED = 0.05;
+const FRICTION = 0.97;
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    player.update()
+
+    if (keys.w.pressed) {
+        player.velocity.x = Math.cos(player.rotation) * SPEED;
+        player.velocity.y = Math.sin(player.rotation) * SPEED;
+    } else if (!keys.w.pressed) {
+        player.velocity.x *= FRICTION;
+        player.velocity.y *= FRICTION;
+    }
+
+    if (keys.d.pressed) player.rotation += ROTATION_SPEED;
+    else if (keys.a.pressed) player.rotation -= ROTATION_SPEED;
+}
+
+animate();
+
+window.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'KeyW':
+            keys.w.pressed = true;
+            break;
+        case 'KeyA':
+            keys.a.pressed = true;
+            break;
+        case 'KeyD':
+            keys.d.pressed = true;
+            break;
+        default:
+            break;
+    }
+});
+
+window.addEventListener('keyup', (event) => {
+    switch (event.code) {
+        case 'KeyW':
+            keys.w.pressed = false;
+            break;
+        case 'KeyA':
+            keys.a.pressed = false;
+            break;
+        case 'KeyD':
+            keys.d.pressed = false;
+            break;
+        default:
+            break;
+    }
+});
